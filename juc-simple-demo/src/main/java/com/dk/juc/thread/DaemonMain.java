e8f51b21d9c1fc50590c0127c725b610fd214b21
dk05408 <dongkun.sh@superjia.com>
2017-06-26 16:41:14 +0800

    StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();

--- juc-simple-demo/src/main/java/com/dk/juc/thread/DaemonMain.java
+++ juc-simple-demo/src/main/java/com/dk/juc/thread/DaemonMain.java
@@ -1,5 +1,8 @@
 package com.dk.juc.thread;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created with IntelliJ IDEA
  * DaemonMain
@@ -8,11 +11,15 @@ package com.dk.juc.thread;
  * @date 2017/6/23 18:37
  */
 public class DaemonMain {
+
+    private static Logger logger = LoggerFactory.getLogger(DaemonMain.class);
+
     private static class DaemonT extends Thread{
+        private Logger logger = LoggerFactory.getLogger(getClass());
         @Override
         public void run() {
             while (true){
-                System.out.println("I am alive");
+                logger.info("I am alive");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -26,6 +33,10 @@ public class DaemonMain {
         Thread t = new DaemonT();
         t.setDaemon(true);//设置t为守护线程
         t.start();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (StackTraceElement element:stackTrace) {
+            logger.info("_______"+element.toString());
+        }
 
         //当主线程main为用户线程，因此在main线程休眠2s后退出，整个程序也随之结束
         //如果不把t设置为守护线程，main线程结束后，t线程还不会不停打印，永远不会结束
