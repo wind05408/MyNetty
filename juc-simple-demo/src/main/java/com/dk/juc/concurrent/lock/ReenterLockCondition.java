ecdcdb0dae2e9a673012a5e207185f4eae614ae6
dk05408 <dongkun.sh@superjia.com>
2017-07-25 15:01:02 +0800

    ReentrantLock Condition

---
+++ juc-simple-demo/src/main/java/com/dk/juc/concurrent/lock/ReenterLockCondition.java
@@ -0,0 +1,44 @@
+package com.dk.juc.concurrent.lock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created with IntelliJ IDEA
+ * ReenterLockCondition
+ *
+ * @author dk
+ * @date 2017/7/25 13:55
+ */
+public class ReenterLockCondition implements Runnable {
+    public static ReentrantLock lock = new ReentrantLock();
+    public static Condition condition = lock.newCondition();
+
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            condition.await();
+            System.out.println("Thread is going on");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLockCondition tl = new ReenterLockCondition();
+        Thread t1 = new Thread(tl);
+        t1.start();
+        Thread.sleep(2000);
+        try {
+            lock.lock();
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}
