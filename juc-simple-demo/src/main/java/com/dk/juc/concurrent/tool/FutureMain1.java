95835be969864f5e849100ba7a940e0d91717892
dk05408 <dongkun.sh@superjia.com>
2017-07-27 14:37:50 +0800

    future

--- juc-simple-demo/src/main/java/com/dk/juc/concurrent/tool/FutureMain1.java
+++ juc-simple-demo/src/main/java/com/dk/juc/concurrent/tool/FutureMain1.java
@@ -1,5 +1,10 @@
 package com.dk.juc.concurrent.tool;
 
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * Created with MyNetty.
  *
@@ -8,4 +13,21 @@ package com.dk.juc.concurrent.tool;
  * @create 2017-07-26 19:29
  **/
 public class FutureMain1 {
+    public static void main(String[] args) {
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return new Random().nextInt(100);
+            }
+        };
+
+        FutureTask<Integer> future = new FutureTask<Integer>(callable);
+        new Thread(future).start();
+        try {
+            Thread.sleep(5000);
+            System.out.println(String.valueOf(future.get()));
+        } catch (InterruptedException|ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
