07a47a5371e1397a4edce2f0a3c250ba7ae6da2a
dk05408 <dongkun.sh@superjia.com>
2017-08-28 15:03:13 +0800

    ThreadPoolExecutor beforeExecute(),afterExecute(),terminated()

---
+++ juc-simple-demo/src/main/java/com/dk/juc/concurrent/pool/ThreadPoolMain.java
@@ -0,0 +1,61 @@
+package com.dk.juc.concurrent.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with MyNetty
+ *
+ * @author dk05408
+ * @Description:
+ * @create 2017-08-28 14:51
+ **/
+public class ThreadPoolMain {
+
+    private static class MyTask implements Runnable{
+        public String name;
+
+        public MyTask(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("正在执行:Thread ID："+Thread.currentThread().getId()+",Task Name ="+name);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = new ThreadPoolExecutor(5,5,
+                0l, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>()){
+            @Override
+            protected void beforeExecute(Thread t, Runnable r) {
+                System.out.println("准备执行："+((MyTask)r).name);
+            }
+
+            @Override
+            protected void afterExecute(Runnable r, Throwable t) {
+                System.out.println("执行完成："+((MyTask)r).name);
+            }
+
+            @Override
+            protected void terminated() {
+                System.out.println("线程池退出");
+            }
+        };
+
+        for (int i = 0; i < 5 ; i++) {
+            MyTask task = new MyTask("TASK-GEYM-"+i);
+            service.execute(task);
+            Thread.sleep(10);
+        }
+        service.shutdown();
+    }
+}
