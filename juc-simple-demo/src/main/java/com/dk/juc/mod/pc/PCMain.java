094e3ee0cdb4c366355c75a99dbb239d26517cb1
dk05408 <dongkun.sh@superjia.com>
2017-09-07 14:53:24 +0800

    pc mode

--- juc-simple-demo/src/main/java/com/dk/juc/mod/pc/PCMain.java
+++ juc-simple-demo/src/main/java/com/dk/juc/mod/pc/PCMain.java
@@ -10,16 +10,20 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Created with MyNetty
  *
  * @author dk05408
- * @Description:
+ * @Description: 生产者-消费者模式 ===>数据在多线程间的共享
  * @create 2017-09-06 19:19
  **/
 public class PCMain {
     public static void main(String[] args) throws InterruptedException {
+        //BlockingQueue共享缓冲区：缓存生产者提交的任务或数据，供消费者使用
         BlockingQueue<PCData> queue = new LinkedBlockingQueue<>(10);
+
+        //生产者：创建和提交数据或任务
         Producer producer1 = new Producer(queue);
         Producer producer2 = new Producer(queue);
         Producer producer3 = new Producer(queue);
 
+        //消费者：处理和消费数据
         Consumer consumer1 = new Consumer(queue);
         Consumer consumer2 = new Consumer(queue);
         Consumer consumer3 = new Consumer(queue);
