72c25d0ee312ff8175eb61130e095534e290060f
dk05408 <dongkun.sh@superjia.com>
2017-09-21 17:35:45 +0800

    ps demo

--- juc-simple-demo/src/main/java/com/dk/juc/mod/ps/SortMain.java
+++ juc-simple-demo/src/main/java/com/dk/juc/mod/ps/SortMain.java
@@ -1,5 +1,7 @@
 package com.dk.juc.mod.ps;
 
+import java.util.Arrays;
+
 /**
  * Created with MyNetty
  *
@@ -8,18 +10,21 @@ package com.dk.juc.mod.ps;
  * @create 2017-09-21 15:08
  **/
 public class SortMain {
+    static int[] arr={1,4,2,6,35,3};
 
     //冒泡排序
     public static void bubbleSort(int[] arr){
-        for (int i = arr.length - 1; i >0 ; i++) {
+        for (int i = arr.length - 1; i >0 ; i--) {
             for (int j = 0; j < i; j++) {
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    System.out.println(Arrays.toString(arr));
                 }
             }
         }
+
     }
 
     //奇偶交换排序的串行实现
@@ -41,5 +46,10 @@ public class SortMain {
                 start = 0;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
     }
 }
